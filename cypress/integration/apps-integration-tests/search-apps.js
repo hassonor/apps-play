4a5fefb3dc9fe24b9d930f1420a731fad9358037
Or Hasson <hassonor@gmail.com>
2022-02-17 19:58:35 +0200

    clean comments

--- cypress/integration/apps-integration-tests/search-apps.js
+++ cypress/integration/apps-integration-tests/search-apps.js
@@ -46,18 +46,3 @@ describe("Test Searching ", () => {
 
 })
 
-// describe("Test Searching Contacts By Age and Name", () => {
-//     it("Should has 1 results with  age of 30 and name of Mitzi Curtis", () => {
-//         cy.visit("http://localhost:3000");
-//         cy.get('.pa3').type("30 Mitzi%20Curtis");
-//         cy.get('[data-cy=find]').click({force: true});
-//         cy.get('img').should('have.length', 1);
-//     });
-//
-//     it("Should has empty results (not contacts) with age of 30 and name of Mitzi Cohen", () => {
-//         cy.visit("http://localhost:3000");
-//         cy.get('.pa3').type("30 Mitzi%20Cohen");
-//         cy.get('[data-cy=find]').click({force: true});
-//         cy.get('img').should('have.length', 0);
-//     });
-// })
